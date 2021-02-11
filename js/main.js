03eda3427ce2f7c1a8ded4452e0150e4a39ff90b
yliia-batova <zayka130192@meta.ua>
2021-02-11 17:24:40 +0200

    added style reviews

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 $(function () {
   $(".intro__slider").slick({
-      arrows: false,
-      dots: true,
-      autoplay: true,
-      fade: true,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    fade: true,
+  });
+  $(".reviews__slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: false,
+    dots: true,
+    autoplay: true,
   });
 });
