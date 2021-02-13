a7b6ada7126b9fe5fb92b5089df761ec3ea433d7
yliia-batova <zayka130192@meta.ua>
2021-02-13 13:06:59 +0200

    added adaptive

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function () {
     arrows: false,
     dots: true,
     autoplay: true,
-    fade: true,
+    fade: true,    
   });
   $(".reviews__slider").slick({
     slidesToShow: 4,
@@ -11,5 +11,32 @@ $(function () {
     arrows: false,
     dots: true,
     autoplay: true,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3  
+        }
+      },
+      {
+        breakpoint: 810,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false
+        }
+      }      
+    ]
+  });
+  $('.header__menu-btn').on('click', function () {
+    $('.menu').slideToggle();
   });
 });
